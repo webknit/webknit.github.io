2186bc1dd37d7b88c6b2faaaa1fb65fecb5f757d
Shane Prendergast <contact@webknit.co.uk>
2023-10-22 22:21:44 +0100

    adding static

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {
+      container: {
+        padding: "1rem",
+      },
+      colors: {
+        "webknit-blue": "#0026ff",
+      },
+      fontFamily: {
+        gtreg: "GTReg",
+        gtbolb: "GTBold",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
